3a241c262688cad69fd0b1f6084b4a23121a2ac3
ZHOUKAN WANG <wang.zhoukan.2022@mumail.ie>
2024-02-27 21:28:00 +0000

    Create all the components of Simon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      111111111111111111111111111
+    <div className = "App">
+      Simon Game Controller
+      <div className = "Dashboard"></div>
+      <div className = "Container">
+        <div className = "GameButton Yellowgreen"></div>
+        <div className = "GameButton Orangered"></div>
+        <div className = "GameButton Gold"></div>
+        <div className = "GameButton Dodgerblue"></div>
+      </div>
+      <div className = "Container XXX">
+        <button className="NumButton">10</button>
+        <button className="NumButton">START</button>
+        <button className="NumButton">04</button>
+      </div>
+      <div className = "Container">
+        <div className = "Indicator Orangered"></div>
+        <div className = "Indicator Yellowgreen"></div>
+      </div>
+
+
     </div>
   );
 }
