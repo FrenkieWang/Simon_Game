b700cc4b93986c35ad23e3b25b0b55242e68b190
ZHOUKAN WANG <wang.zhoukan.2022@mumail.ie>
2024-02-28 11:28:39 +0000

    Finish Simon Game UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,28 +2,27 @@ import React from 'react';
 import './App.css'
 
 function App() {
+  
   return (
     <div className = "App">
       Simon Game Controller
-      <div className = "Container">
-        <div className = "Dashboard Black Circle"></div>
-        <div className = "Hidden Circle"></div>
-      </div>
-      <div className = "Container">
-        <div className = "GameButton Green Circle"></div>
-        <div className = "GameButton Red Circle"></div>
-        <div className = "GameButton Yellow Circle"></div>
-        <div className = "GameButton Blue Circle"></div>
-      </div>
-      <div className = "Container XXX">
-        <button className="NumButton">10</button>
-        <button className="NumButton">START</button>
-        <button className="NumButton">04</button>
-      </div>
-      <div className = "Container">
-        <div className = "Indicator Orangered"></div>
-        <div className = "Indicator Yellowgreen"></div>
-      </div>
+
+      <div className = "Simon UI">
+        <div className = "Dashboard Circle">
+          <div className = "ScoreBoard-Bar">
+            <button className="ScoreBoard Score">10</button>
+            <button className="ScoreBoard CtrlBtn">START</button>
+            <button className="ScoreBoard Score">04</button>
+          </div>
+
+          <div className="Indicator Circle"></div> 
+
+          <div className="GameButton Green Circle"></div>
+          <div className="GameButton Red Circle"></div>
+          <div className="GameButton Yellow Circle"></div>
+          <div className="GameButton Blue Circle"></div>
+        </div>
+      </div> {/* end of Simon UI */}
     </div>
   );
 }
